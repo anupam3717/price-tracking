eebc5b87b0080797aaf419933d26c4861c6a2762
Anupam <anupam3717@gmail.com>
2023-04-28 00:10:31 +0530

    final

--- src/main/java/com/example/price/tracking/controller/GetPrice.java
+++ src/main/java/com/example/price/tracking/controller/GetPrice.java
@@ -1,8 +1,11 @@
 package com.example.price.tracking.controller;
 
+import com.example.price.tracking.dto.Error;
+import com.example.price.tracking.dto.PriceDto;
 import com.example.price.tracking.service.PriceImplement;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,12 +17,19 @@ public class GetPrice {
     @Autowired
     private PriceImplement price;
     @GetMapping("/price")
-    public String getPrice(){
-        return price.getLatestPrice();
+    public ResponseEntity<?> getPrice(){
+        PriceDto priceDto=PriceDto.builder()
+                .price(price.getLatestPrice())
+                .build();
+
+        return ResponseEntity.ok().body(priceDto);
     }
 
     @RequestMapping("/**")
-    public String fallback() {
-        return "Invalid endpoint!";
+    public ResponseEntity<?> fallback() {
+        Error error=Error.builder()
+                .error("Invalid endpoint!")
+                .build();
+        return ResponseEntity.status(404).body(error);
     }
 }
---
+++ src/main/java/com/example/price/tracking/dto/PriceDto.java
@@ -0,0 +1,11 @@
+package com.example.price.tracking.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+@Builder
+@Setter
+@Getter
+public class PriceDto {
+    private String price;
+}
